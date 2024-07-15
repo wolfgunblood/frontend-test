868e719749618a4a1e939dbbeea40017754e8716
wolfgunblood <jalajdorai1@gmail.com>
2024-07-15 13:43:24 +0530

    app init

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { getServerAuthSession } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  void api.post.getLatest.prefetch();
+
+  return (
+    <HydrateClient>
+      <main>
+        <h1 className="text-red-500">
+          Hello World!!!
+        </h1>
+      </main>
+    </HydrateClient>
+  );
+}
