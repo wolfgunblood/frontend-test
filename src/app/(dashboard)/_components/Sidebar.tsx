a75640956e57680de5f7e0939d6941a31e4496ed
wolfgunblood <jalajdorai1@gmail.com>
2024-07-15 22:31:02 +0530

    sidebar styling

--- src/app/(dashboard)/_components/Sidebar.tsx
+++ src/app/(dashboard)/_components/Sidebar.tsx
@@ -7,7 +7,16 @@ import SidebarRoutesBottom from './SidebarRoutesBottom'
 
 const Sidebar = () => {
   return (
-    <div className='h-full border-r flex flex-col p-5 justify-between overflow-y-auto shadow-sm'>
+    <div
+    style={{
+      width: '320px', 
+      // height: '1286px', 
+      padding: '32px 0 0 0',
+      borderRight: '1px solid',
+    
+    }}
+    className='flex flex-col justify-between overflow-y-auto shadow-sm'
+  >
       <div>
         <div className='flex flex-col gap-2'>
           <Button>
