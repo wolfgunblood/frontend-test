8300d9d619eabdbefb8852e7c0145a50e122303c
wolfgunblood <jalajdorai1@gmail.com>
2024-07-16 21:03:57 +0530

    updated chart

--- src/app/(dashboard)/_components/Chart.tsx
+++ src/app/(dashboard)/_components/Chart.tsx
@@ -1,3 +1,4 @@
+import { ArrowUp } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -5,13 +6,30 @@ const Chart = () => {
     return (
         <div className='flex flex-col gap-4 items-center'>
             <div className="w-full max-w-[256px] h-[201.74px] p-4 border border-zinc-200 rounded-2xl">
-                <Image
-                    src="/chart-widget.svg"
-                    alt="Chart"
-                    width={256}
-                    height={202} 
-                    className="object-cover w-full h-full"
-                />
+                <div className='flex flex-col justify-between'>
+                    <div className='flex justify-between'>
+                        <div>
+                            <h2 className='text-base text-zinc-800 font-semibold font-manrope'>
+                                Weekly Plays
+                            </h2>
+                            <p className='text-2xl text-zinc-800 font-bold font-manrope'>
+                                738,458
+                            </p>
+                        </div>
+                        <div className='inline-flex items-center gap-2'>
+                            <ArrowUp size={16} color='#16A34A' />
+                            <p>17%</p>
+                        </div>
+                    </div>
+
+                    <Image
+                        src="/Graph.svg"
+                        alt="graph"
+                        width={256}
+                        height={202}
+                        className="object-fit"
+                    />
+                </div>
             </div>
 
             <div className='flex gap-3'>
