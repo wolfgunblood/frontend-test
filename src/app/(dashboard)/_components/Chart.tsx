c96efad4ceb8c254a6c2d2aec2116ef426efe73a
wolfgunblood <jalajdorai1@gmail.com>
2024-07-16 11:04:17 +0530

    sidebar done

--- src/app/(dashboard)/_components/Chart.tsx
+++ src/app/(dashboard)/_components/Chart.tsx
@@ -3,16 +3,39 @@ import React from 'react'
 
 const Chart = () => {
     return (
-        <div className='flex flex-col gap-4'>
-
-        <Image
-            src="/chart-widget.svg"
-            alt="Chart"
-            width={32}
-            height={32}
-            className="w-8 h-8 rounded-sm"
-            />
+        <div className='flex flex-col gap-4 items-center'>
+            <div className="w-full max-w-[256px] h-[201.74px] p-t-4 border-t border-gray-200 rounded-tl-[16px] flex gap-2">
+                <Image
+                    src="/chart-widget.svg"
+                    alt="Chart"
+                    width={256} // Using the native width attribute for accurate rendering
+                    height={202} // Using rounded height to match CSS requirements
+                    className="object-cover w-full h-full rounded-tl-lg"
+                />
             </div>
+
+            <div className='flex gap-3'>
+                <Image
+                    src="/Rectangle.svg"
+                    alt="Rectangle"
+                    width={8}
+                    height={8}
+                />
+                <Image
+                    src="/Ellipse6.svg"
+                    alt="Ellipse"
+                    width={8}
+                    height={8}
+                />
+                <Image
+
+                    src="/Ellipse7.svg"
+                    alt="Ellipse"
+                    width={8}
+                    height={8}
+                />
+            </div>
+        </div>
     )
 }
 
