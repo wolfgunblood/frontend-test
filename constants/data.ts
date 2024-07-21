c133acde1ecfe3bd6a436e55c00a325b1c27dc70
wolfgunblood <jalajdorai1@gmail.com>
2024-07-21 21:10:40 +0530

    updated the data

--- constants/data.ts
+++ constants/data.ts
@@ -4,7 +4,7 @@ export const options = [
     name: "Eight Sleep Q4 Pod 3 - v1",
     createdOn: "13/03/24 • 3m 17s",
     picture:
-    "https://s3-alpha-sig.figma.com/img/8ddc/2952/d1325f32c824d8c79646c992c60f95db?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dIfmwl0nZt9B9lCM99SsT0X-mnHcWPCzOy1a65x86i4pK6okKi6ZxYKlFgUBG34VeFBivV0MvHn1K8UGCI2SXOEOpLzjwokUnkasX3mpLguhtnW5P9T0m1FwR~aZVrX8hylSlXMM~lfBrujNX5pAqazNufHHYrGG6PDD4s1zlRXu4EqQ-PkbquicH4zL713SKxvkHQTCsBVIcTFlokDqdYTTO5Z3S8Ab5FR6hkt4VhVomLbHbYQfONQ9zs2C6wOYvnCjgAkjUNTbW5q-Q7sxEiDlhZYFimDPg1Yfy~vahH~XDDfbJ95JvhFxNfCrcHwNL7IQNo1hlaXn95WV7nBpAg__",
+      "https://s3-alpha-sig.figma.com/img/8ddc/2952/d1325f32c824d8c79646c992c60f95db?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dIfmwl0nZt9B9lCM99SsT0X-mnHcWPCzOy1a65x86i4pK6okKi6ZxYKlFgUBG34VeFBivV0MvHn1K8UGCI2SXOEOpLzjwokUnkasX3mpLguhtnW5P9T0m1FwR~aZVrX8hylSlXMM~lfBrujNX5pAqazNufHHYrGG6PDD4s1zlRXu4EqQ-PkbquicH4zL713SKxvkHQTCsBVIcTFlokDqdYTTO5Z3S8Ab5FR6hkt4VhVomLbHbYQfONQ9zs2C6wOYvnCjgAkjUNTbW5q-Q7sxEiDlhZYFimDPg1Yfy~vahH~XDDfbJ95JvhFxNfCrcHwNL7IQNo1hlaXn95WV7nBpAg__",
     createdBy: {
       picture:
         "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
@@ -23,7 +23,7 @@ export const options = [
       "https://s3-alpha-sig.figma.com/img/58d8/bc90/539d6fda456af21a4312573096caca17?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RofoN~EmRrrwBp7deqB5L-~I9m7uRfzhRctEskztbeJbiVMteJ1huwpFr39-~6bSEosP-eksZjAncFaXb7Jz8gy~RKzQXwq1voE6Ew9uO3l7dHhxHrCrsFmC1Mn3nDGqjFYHdH~A2VvJbHQQd4FTt-kQeYFJ4HLQcUr~wZvXWEZkX07XOPZnelcL5p9DwghC7lr4a1omHhDCamzX1BGoYvKcfGU67ku5oiJ4PDmpQoQmI40nDgdZRVTISw0TP0sVybMUbGRWi-pGS3orB6BuBl7HhIwOSiKKaTK7Vye17Y1gSEMwar0~6OEUbxlUGyAe2ZTQDgGt0CD2E-jPkQUQIA__",
     createdBy: {
       picture:
-      "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
+        "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
       name: "Denis Loginoff",
     },
     badge: {
@@ -36,15 +36,15 @@ export const options = [
     name: "Brilliant(maths & physics)",
     createdOn: "13/03/24 • 3m 17s",
     picture:
-      "https://s3-alpha-sig.figma.com/img/3903/4047/1a5ec8059b23e2cd1967a5c98ff1bae3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MN0nUgh73Oh7IZQGtAc2pm1yYHcLlfWLQFIKhYvwyed1yptgCmJo~090KdaGyALZlqNt7RsfOf9vmk9ld0-c4m-bXX8sqIc5x2CfLhTzawNIjquzWz-id7usZSqfC4DJabOse4WKKRpI2-4dUZg6zSsd4bm~Iubm0mgI8~2ATcO4huriXBKWANSAvpsjCsqScjJkFYA9A-ozM33YZwodbEf8f5PrmcRFWxFEyoiEHFTgnWL2Ss9VkNNC5nQQjelje4nbHMPOjavrUpR5nSp81geJAMEOqlTsDwWsVKhG9AEc6YKXoDNylPMx8gwX5z6MeHVVc16t3DuY70z8To98cw__",
+      "https://s3-alpha-sig.figma.com/img/180b/9b10/09d2aa6aa159c6ade1c20df197fd0ffb?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LW39omOsr12R3Lr549JdA0e3viux8JPVrCJakbaiA09~hnYuBTCL4MytgPFpcrmZ981HVuX53eCecga8ETPeYY4sdbGD-xBA6RlSmNiCwfP8c6y6kGkBgt7RBYXipTIp3QOPJyElB80LGm8z2uz53xnFHwlP9CHN9ufr8Gx1oB0CP~EXVu4Q8pTYI0GUWOQLs4NEpY23EEK3y-e5df4R2iD9jhHJBw6jn-Yg~tfPV1xzbvfwO9S95oS7zzxLcEA9tYiDAUsNTKhgkelTQAgbGfLwZNYvC4SivfH1i2l08F8ZBApMuJ29kvOwb6Lx5kg9J3GP0OO9IWnVG6vA3H4rZA__",
     createdBy: {
       picture:
         "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
       name: "Denis Loginoff",
     },
     badge: {
-      category: "Eight Sleep",
-      subCategory: "Pod 3",
+      category: "Brilliant",
+      subCategory: "Back to school 24",
     },
   },
   {
@@ -55,7 +55,7 @@ export const options = [
       "https://s3-alpha-sig.figma.com/img/3903/4047/1a5ec8059b23e2cd1967a5c98ff1bae3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MN0nUgh73Oh7IZQGtAc2pm1yYHcLlfWLQFIKhYvwyed1yptgCmJo~090KdaGyALZlqNt7RsfOf9vmk9ld0-c4m-bXX8sqIc5x2CfLhTzawNIjquzWz-id7usZSqfC4DJabOse4WKKRpI2-4dUZg6zSsd4bm~Iubm0mgI8~2ATcO4huriXBKWANSAvpsjCsqScjJkFYA9A-ozM33YZwodbEf8f5PrmcRFWxFEyoiEHFTgnWL2Ss9VkNNC5nQQjelje4nbHMPOjavrUpR5nSp81geJAMEOqlTsDwWsVKhG9AEc6YKXoDNylPMx8gwX5z6MeHVVc16t3DuY70z8To98cw__",
     createdBy: {
       picture:
-      "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
+        "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
       name: "Denis Loginoff",
     },
     badge: {
@@ -71,7 +71,7 @@ export const options = [
       "https://s3-alpha-sig.figma.com/img/58d8/bc90/539d6fda456af21a4312573096caca17?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RofoN~EmRrrwBp7deqB5L-~I9m7uRfzhRctEskztbeJbiVMteJ1huwpFr39-~6bSEosP-eksZjAncFaXb7Jz8gy~RKzQXwq1voE6Ew9uO3l7dHhxHrCrsFmC1Mn3nDGqjFYHdH~A2VvJbHQQd4FTt-kQeYFJ4HLQcUr~wZvXWEZkX07XOPZnelcL5p9DwghC7lr4a1omHhDCamzX1BGoYvKcfGU67ku5oiJ4PDmpQoQmI40nDgdZRVTISw0TP0sVybMUbGRWi-pGS3orB6BuBl7HhIwOSiKKaTK7Vye17Y1gSEMwar0~6OEUbxlUGyAe2ZTQDgGt0CD2E-jPkQUQIA__",
     createdBy: {
       picture:
-      "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
+        "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
       name: "Denis Loginoff",
     },
     badge: {
@@ -79,4 +79,37 @@ export const options = [
       subCategory: "Pod 3",
     },
   },
+  {
+    id: "6",
+    name: "Brilliant(biology)",
+    createdOn: "13/03/24 • 3m 17s",
+    picture:
+      "https://s3-alpha-sig.figma.com/img/3903/4047/1a5ec8059b23e2cd1967a5c98ff1bae3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MN0nUgh73Oh7IZQGtAc2pm1yYHcLlfWLQFIKhYvwyed1yptgCmJo~090KdaGyALZlqNt7RsfOf9vmk9ld0-c4m-bXX8sqIc5x2CfLhTzawNIjquzWz-id7usZSqfC4DJabOse4WKKRpI2-4dUZg6zSsd4bm~Iubm0mgI8~2ATcO4huriXBKWANSAvpsjCsqScjJkFYA9A-ozM33YZwodbEf8f5PrmcRFWxFEyoiEHFTgnWL2Ss9VkNNC5nQQjelje4nbHMPOjavrUpR5nSp81geJAMEOqlTsDwWsVKhG9AEc6YKXoDNylPMx8gwX5z6MeHVVc16t3DuY70z8To98cw__",
+    createdBy: {
+      picture:
+        "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
+      name: "Denis Loginoff",
+    },
+    badge: {
+      category: "Eight Sleep",
+      subCategory: "Pod 3",
+    },
+  },
+  {
+    id: "7",
+    name: "Brilliant(maths & physics)",
+    createdOn: "13/03/24 • 3m 17s",
+    picture:
+      "https://s3-alpha-sig.figma.com/img/180b/9b10/09d2aa6aa159c6ade1c20df197fd0ffb?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LW39omOsr12R3Lr549JdA0e3viux8JPVrCJakbaiA09~hnYuBTCL4MytgPFpcrmZ981HVuX53eCecga8ETPeYY4sdbGD-xBA6RlSmNiCwfP8c6y6kGkBgt7RBYXipTIp3QOPJyElB80LGm8z2uz53xnFHwlP9CHN9ufr8Gx1oB0CP~EXVu4Q8pTYI0GUWOQLs4NEpY23EEK3y-e5df4R2iD9jhHJBw6jn-Yg~tfPV1xzbvfwO9S95oS7zzxLcEA9tYiDAUsNTKhgkelTQAgbGfLwZNYvC4SivfH1i2l08F8ZBApMuJ29kvOwb6Lx5kg9J3GP0OO9IWnVG6vA3H4rZA__",
+    createdBy: {
+      picture:
+        "https://s3-alpha-sig.figma.com/img/a8e6/c37f/49d009c9ebf0260096dbdef6364b5b37?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cKmbyIkzRuUBvGUb6L3PB9S6OUWSIJLGcHiynzTFUsGE-TATskui6TBL007fEqy0qPVhT5bERSa9Y5KudXSXfHZb~zpMqxeHL4BnPIQKgqg9xlWRADo5TC1JKuJrHL~9kflzzdmde4GpAeK3z43KJyOIZgzpwnXyVWR3PWDEkEmVdIpRCFBLKo~CGxjNlrKvQ5UqgHQggvyh7RY710SIJQ83R-BosjPaXt1X0~22FNbOFp9-k4hFzZYV~sSRf-vcfb~upMNUEvtO6Ee~tfgB~sXEiL7cHFB~FUiHH8DNcIXTIgw1Ou12pIH89q0auNgzqh6VkrK2Ec6DWrzAPR3vHA__",
+      name: "Denis Loginoff",
+    },
+    badge: {
+      category: "Brilliant",
+      subCategory: "Back to school 24",
+    },
+  },
+ 
 ];
