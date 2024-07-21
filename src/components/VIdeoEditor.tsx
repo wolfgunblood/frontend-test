799cf9e2fc1c7be5420490a7f0a773d1f00d5247
wolfgunblood <jalajdorai1@gmail.com>
2024-07-21 17:43:21 +0530

    change video

--- src/components/VIdeoEditor.tsx
+++ src/components/VIdeoEditor.tsx
@@ -106,8 +106,8 @@ const VideoEditor: React.FC = () => {
                     <div className="rounded-lg overflow-hidden ">
                         <ReactPlayer
                             ref={playerRef}
-                            // url='https://utfs.io/f/37835069-4b3e-48e8-97b5-da654c1de85b-m0d2yz.mp4'
-                            url='https://www.youtube.com/watch?v=V0ej29G7ZGg&t=1694s'
+                            url='https://utfs.io/f/37835069-4b3e-48e8-97b5-da654c1de85b-m0d2yz.mp4'
+                            // url='https://www.youtube.com/watch?v=V0ej29G7ZGg&t=1694s'
                             controls={true}
                             playing={playing}
                             onProgress={onProgress}
